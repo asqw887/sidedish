9f1e1b8efafdda69e5d9c9f3b22b7bacf7adadd0
asqw887 <asqw887@gmail.com>
2022-05-02 14:44:25 +0900

    FEAT: get요청 메소드 구현

--- sideDish/sideDish/NetworkManager.swift
+++ sideDish/sideDish/NetworkManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import OSLog
+
+enum HttpMethod: String{
+    case get = "GET"
+}
+
+class NetworkManager{
+    
+    // getRequest
+    static func getRequest(url: String, complete: @escaping (Data) -> Void){
+        guard let validURL = URL(string: url) else { return }
+        var urlRequest = URLRequest(url: validURL)
+        urlRequest.httpMethod = HttpMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: urlRequest){ data, response, _ in
+            guard let data = data else { return }
+            guard let response = response as? HTTPURLResponse, (200..<300).contains(response.statusCode) else {
+                if let response = response as? HTTPURLResponse{
+                    os_log("%@", "\(response.statusCode)")
+                }
+                return
+            }
+            
+            complete(data)
+        }.resume()
+    }
+    
+}
